b6fbc21f8d1688ee4d4c8b99f39bc7d5b34a5428
LeHieu11 <letrantrunghieu11@gmail.com>
2021-12-22 15:12:53 +0700

    Sửa hiện tất cả nhân viên ở giám đốc

--- giamdoc_db.php
+++ giamdoc_db.php
@@ -4,9 +4,13 @@
     function getAllEmployees(){
         $conn = getConnection();
 
-        $sql = "SELECT MA_NV, HO_TEN, TEN_PB, CHUC_VU
+        $sql = "SELECT MA_NV, HO_TEN, TEN_PB,   CASE
+                                                    WHEN EXISTS (SELECT MA_NV FROM truong_phong WHERE user_info.MA_NV = truong_phong.MA_NV)
+                                                        THEN 'Tổ Trưởng'
+                                                    ELSE 'Nhân Viên'
+                                                END AS CHUC_VU
                 FROM user, user_info, phong_ban
-                WHERE user.MA_USER = user_info.MA_NV AND phong_ban.MA_PHONG_BAN = user_info.MA_PHONG_BAN 
+                WHERE user.MA_USER = user_info.MA_NV AND phong_ban.MA_PHONG_BAN = user_info.MA_PHONG_BAN  AND phong_ban.MA_PHONG_BAN AND user_info.CHUC_VU != 'admin'  
                 ORDER BY MA_NV ASC";
 
         $stm = $conn->prepare($sql);
