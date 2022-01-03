59ecf185abb26cdac81c09baa51b29632473fa04
LeHieu11 <letrantrunghieu11@gmail.com>
2022-01-03 16:01:00 +0700

    Thêm cột task_id để dễ sử lý sang chi tiết

--- render_task_db.php
+++ render_task_db.php
@@ -2,7 +2,7 @@
 require_once('db.php');
 
 function get_task_employee($id){
-    $sql = "SELECT `task`.`TIEU_DE`, `task_info`.`MA_NGUOI_NHAN`, `user`.`HO_TEN`, `task_info`.`STATUS`, `task`.`DEADLINE` FROM `task`, `task_info`, `user`, `user_info` WHERE `task_info`.`STATUS` != 'Completed' AND `task_info`.`STATUS` != 'Canceled' AND `task_info`.`MA_NGUOI_NHAN` = $id AND `user`.`MA_USER` = `user_info`.`MA_USER` AND `task`.`TASK_ID` = `task_info`.`TASK_ID` AND `task_info`.`MA_NGUOI_NHAN` = `user`.`MA_USER`";
+    $sql = "SELECT `task`.`TASK_ID`, `task`.`TIEU_DE`, `task_info`.`MA_NGUOI_NHAN`, `user`.`HO_TEN`, `task_info`.`STATUS`, `task`.`DEADLINE` FROM `task`, `task_info`, `user`, `user_info` WHERE `task_info`.`STATUS` != 'Completed' AND `task_info`.`STATUS` != 'Canceled' AND `task_info`.`MA_NGUOI_NHAN` = $id AND `user`.`MA_USER` = `user_info`.`MA_USER` AND `task`.`TASK_ID` = `task_info`.`TASK_ID` AND `task_info`.`MA_NGUOI_NHAN` = `user`.`MA_USER`";
     $conn = getConnection();
     $result = $conn->query($sql);
     $output = array();
@@ -14,7 +14,7 @@ function get_task_employee($id){
 
 
 function get_completed_task_employee($id){
-    $sql = "SELECT `task`.`TIEU_DE`, `task_info`.`MA_NGUOI_NHAN`, `user`.`HO_TEN`, `task_info`.`STATUS`, `task_info`.`COMPLETE_STATUS` ,`task`.`DEADLINE` FROM `task`, `task_info`, `user`, `user_info` WHERE `task_info`.`STATUS` = 'Completed' AND `task_info`.`MA_NGUOI_NHAN` = $id AND `user`.`MA_USER` = `user_info`.`MA_USER` AND `task`.`TASK_ID` = `task_info`.`TASK_ID` AND `task_info`.`MA_NGUOI_NHAN` = `user`.`MA_USER`";
+    $sql = "SELECT `task`.`TASK_ID`, `task`.`TIEU_DE`, `task_info`.`MA_NGUOI_NHAN`, `user`.`HO_TEN`, `task_info`.`STATUS`, `task_info`.`COMPLETE_STATUS` ,`task`.`DEADLINE` FROM `task`, `task_info`, `user`, `user_info` WHERE `task_info`.`STATUS` = 'Completed' AND `task_info`.`MA_NGUOI_NHAN` = $id AND `user`.`MA_USER` = `user_info`.`MA_USER` AND `task`.`TASK_ID` = `task_info`.`TASK_ID` AND `task_info`.`MA_NGUOI_NHAN` = `user`.`MA_USER`";
     $conn = getConnection();
     $result = $conn->query($sql);
     $output = array();
@@ -27,7 +27,7 @@ function get_completed_task_employee($id){
 
 function get_task_teamLead($id){
     $conn = getConnection();
-    $sql = "SELECT TIEU_DE, MA_NGUOI_NHAN, HO_TEN, DEADLINE, STATUS
+    $sql = "SELECT t.TASK_ID, TIEU_DE, MA_NGUOI_NHAN, HO_TEN, DEADLINE, STATUS
     FROM task t join task_info ti on t.TASK_ID = ti.TASK_ID
     join user on MA_NGUOI_NHAN = MA_USER
     where MA_NGUOI_GIAO = $id and STATUS not in ('canceled', 'completed')";
@@ -42,7 +42,7 @@ function get_task_teamLead($id){
 
 function get_cancel_task_teamLead($id){
     $conn = getConnection();
-    $sql = "SELECT TIEU_DE, MA_NGUOI_NHAN, HO_TEN, DEADLINE, STATUS
+    $sql = "SELECT t.TASK_ID, TIEU_DE, MA_NGUOI_NHAN, HO_TEN, DEADLINE, STATUS
     FROM task t join task_info ti on t.TASK_ID = ti.TASK_ID
     join user on MA_NGUOI_NHAN = MA_USER
     where MA_NGUOI_GIAO = $id and STATUS = 'canceled'";
@@ -57,7 +57,7 @@ function get_cancel_task_teamLead($id){
 
 function get_completed_task_Tlead($id){
     $conn = getConnection();
-    $sql = "SELECT TIEU_DE, MA_NGUOI_NHAN, HO_TEN, DEADLINE, STATUS
+    $sql = "SELECT t.TASK_ID, TIEU_DE, MA_NGUOI_NHAN, HO_TEN, DEADLINE, STATUS
     FROM task t join task_info ti on t.TASK_ID = ti.TASK_ID
     join user on MA_NGUOI_NHAN = MA_USER
     where MA_NGUOI_GIAO = $id and STATUS = 'completed'";
