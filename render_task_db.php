12869a64209d5617fa317e6c66b4eb1796dfa012
LeHieu11 <letrantrunghieu11@gmail.com>
2022-01-03 22:46:47 +0700

    Bổ sung waiting_task cho cả tlead và employee, chưa sử lý download submit và 2 nút reject, complete

--- render_task_db.php
+++ render_task_db.php
@@ -69,6 +69,20 @@ function get_completed_task_Tlead($id){
     return $output;
 }
 
+function get_task($taskId){
+    $conn = getConnection();
+    $sql = "SELECT user.HO_TEN, task.TIEU_DE, task.MO_TA, task.DEADLINE, task.SUPPORT_FOLDER_PATH, task.SUBMIT_FOLDER_PATH, task.message_employee,
+            task.message_tlead
+            FROM task, user, task_info
+            WHERE task.TASK_ID = task_info.TASK_ID AND user.MA_USER = task_info.MA_NGUOI_GIAO AND task.TASK_ID = $taskId";
 
+    $stm = $conn->prepare($sql);
+    if (!$stm->execute()) {
+        return 0;
+    }
+    $result = $stm->get_result();
+
+    return $result->fetch_all(MYSQLI_ASSOC);
+}
 
 ?>
