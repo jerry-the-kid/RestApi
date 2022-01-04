a9e7acbbb860947c77be3fa2a514ae660398bfa6
jerry-the-kid <jerrycarry79@gmail.com>
2022-01-04 17:45:15 +0700

    New task handler success

--- TLead/new_task_info.php
+++ TLead/new_task_info.php
@@ -55,6 +55,9 @@
 </nav>
 
 <!--Container-->
+<div class="container" id="alert-container">
+
+</div>
 <div class="container">
     <div class="row mb-2 flex-column-reverse flex-md-row">
         <div class="col-md-6 col-12 mb-4 align-items-center justify-content-end pr-3">
@@ -85,12 +88,88 @@
             </ul>
         </div>
         <div class="mt-4 col-md-4 col-lg-6 d-flex justify-content-end align-items-end">
-            <button class="btn btn-secondary">Hủy task</button>
-            <button class="btn btn-success ml-2">Cập nhật</button>
+            <button class="btn btn-secondary" data-toggle="modal" data-target="#cancelModal">Hủy task</button>
+            <button class="btn btn-success ml-2 btn_update" data-toggle="modal" data-target="#updateTaskModal">Cập
+                nhật
+            </button>
         </div>
     </div>
 </div>
 
+
+<div class="modal fade" id="cancelModal" tabindex="-1" role="dialog" aria-labelledby="cancelModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="cancelModalLabel">Hủy task</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Bạn chắc sẽ muốn hủy task này chứ ?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-danger cancel_btn">Cancel task</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--Update Modal-->
+<div class="modal fade" id="updateTaskModal" tabindex="-1" role="dialog" aria-labelledby="updateTaskModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="updateTaskModalLabel">Cập nhật task</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="updateTaskForm">
+                    <div class="form-group">
+                        <label for="task-name" class="col-form-label">Tiêu đề</label>
+                        <input name="task" type="text" class="form-control" id="task-name">
+                    </div>
+                    <div class="form-group">
+                        <label for="deadline" class="col-form-label">Deadline:</label>
+                        <input name="deadline" type="datetime-local" id="deadline_form" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="createTaskNVien">Nhân viên nhận :</label>
+                        <select name="nhanvien" class="form-control" id="createTaskNVien">
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="describe-text" class="col-form-label">Mô tả</label>
+                        <textarea name="describe" class="form-control" id="describe-text"></textarea>
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">File thay đổi</span>
+                        </div>
+                        <div class="custom-file">
+                            <input type="file" name="file" class="custom-file-input" id="inputGroupFile01">
+                            <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                        </div>
+                    </div>
+                    <div class="alert-modal-container">
+
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-success btn-update-task">Cập nhật</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
@@ -105,6 +184,16 @@
 
 <script>
     const taskId = <?php echo $_GET['task']?>;
+    const user_id = 3;
+    let dataStored;
+
+    const alertSuccess = function (message) {
+        const alert = `<div class="alert alert-success" role="alert">
+         ${message}
+         </div>`;
+        $('#alert-container').append(alert);
+        $('.alert-success').fadeOut(3500);
+    }
 
     const createDateFormat = function (date, options) {
         const locale = navigator.language;
@@ -114,34 +203,145 @@
         ).format(new Date(date));
     }
 
-    $(document).ready(function () {
-        const renderData = function (data) {
-            const fileHtmlMarkup = `<li class="list-group-item d-flex justify-content-between align-items-center">${data.SUPPORT_FOLDER_PATH.split('/').slice(-1)}
-                    <a href= "../api/download.php?file=${data.SUPPORT_FOLDER_PATH}" class="btn btn-primary btn-sm">Download</a>
-                </li>`;
-            $('#tieu-de').text(`${data.TIEU_DE}`);
-            $('#name').html(data.HO_TEN);
-            $('#date-created').html(createDateFormat(data.DATE_CREATE, {
-                day: 'numeric',
-                month: 'long',
-                year: 'numeric'
-            }));
-            $('#deadline').html(createDateFormat(data.DEADLINE, {
-                day: 'numeric',
-                month: 'numeric',
-                year: 'numeric',
-                hour: 'numeric',
-                minute: 'numeric',
-            }));
-            $('#describe').html(data.MO_TA);
-            $('#download-container').prepend(fileHtmlMarkup);
-        }
+    const alertFormDanger = function (container, message) {
+        $(container).html('');
+        $(container).append(`<div class="alert alert-danger alert-dismissible fade show" role="alert" id="modal-alert">
+        ${message}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>`);
+    }
 
+    const renderModalInput = function (data) {
+        $('#task-name').val(data.TIEU_DE);
+        const dateControl = document.querySelector('#deadline_form');
+        dateControl.value = data.DEADLINE;
+
+        $('#describe-text').val(data.MO_TA);
+
+    }
+
+    const renderData = function (data) {
+        const fileHtmlMarkup = `<li class="list-group-item d-flex justify-content-between align-items-center">${data.SUPPORT_FOLDER_PATH.split('/').slice(-1)}
+                    <a href= "../api/download.php?file=${data.SUPPORT_FOLDER_PATH}" class="btn btn-primary btn-sm">Download</a>
+                </li>
+                <li class="list-group-item text-center">
+                    <a href="">Download All <i class="ml-2 fas fa-download"></i></a>
+                </li>`;
+        $('#tieu-de').text(`${data.TIEU_DE}`);
+        $('#name').html(data.HO_TEN);
+        $('#date-created').html(createDateFormat(data.DATE_CREATE, {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric'
+        }));
+        $('#deadline').html(createDateFormat(data.DEADLINE, {
+            day: 'numeric',
+            month: 'numeric',
+            year: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+        }));
+        $('#describe').html(data.MO_TA);
+        $('#download-container').html('');
+        $('#download-container').prepend(fileHtmlMarkup);
+    }
+    const loadData = function () {
         $.get('../api/get-task.php', {id: taskId}).done(function (response) {
             const data = response.data[0];
-            console.log(data);
+            dataStored = data;
             renderData(data);
         });
+    }
+
+    $(document).ready(function () {
+        loadData();
+        const taskName = $('#task-name');
+        const deadLine = $('#deadline_form');
+        const nhanVien = $('#createTaskNVien');
+        const describeText = $('#describe-text');
+        const file = $('#inputGroupFile01');
+
+        $('.cancel_btn').on('click', function (e) {
+            $.post('../api/cancel_task.php', {id: taskId}).done(function (response) {
+                console.log(response);
+                $('#cancelModal').modal('hide');
+                document.location.href = 'http://localhost/final/Tlead/task_list.html';
+            });
+        });
+
+        $('.btn_update').on('click', function (e) {
+            renderModalInput(dataStored);
+            const selector = $('#createTaskNVien');
+            $.post('http://localhost/final/API/get_employee_by_Tlead.php', {id: user_id}).done(function (data) {
+                const nhanVien = data.data;
+                if (!nhanVien) {
+                    selector.append('')
+                    selector.attr('disabled', true);
+                    return;
+                }
+                selector.attr('disabled', false);
+                selector.html('');
+                nhanVien.forEach(function (el) {
+                    $('#createTaskNVien').append(`<option value="${el.MA_NV}">${el.HO_TEN} - ${el.MA_NV}</option>`);
+                });
+            });
+        })
+
+        $('.btn-update-task').on('click', function (e) {
+            if (!taskName.val()) {
+                alertFormDanger('.alert-modal-container', 'Tiêu đề còn thiếu. Vui lòng nhập');
+                taskName.focus();
+            } else if (!deadLine.val()) {
+                alertFormDanger('.alert-modal-container', 'Hạn nộp còn thiếu. Vui lòng nhập');
+                deadLine.focus();
+            } else if (!nhanVien.val()) {
+                alertFormDanger('.alert-modal-container', 'Phòng ban không có nhân viên. Vui lòng thêm');
+            } else if (!describeText.val()) {
+                alertFormDanger('.alert-modal-container', 'Vui lòng nhập mô tả task.');
+                describeText.focus();
+            } else {
+                const form = $('#updateTaskForm')[0];
+                const formData = new FormData(form);
+                formData.append("task_id", taskId);
+                formData.append('old_file_path', dataStored.SUPPORT_FOLDER_PATH);
+                $.ajax({
+                    type: "POST",
+                    enctype: 'multipart/form-data',
+                    url: "../api/update_new_task.php",
+                    data: formData,
+                    processData: false,
+                    contentType: false,
+                    cache: false,
+                    timeout: 600000,
+                    success: function (data) {
+                        console.log("SUCCESS : ", data);
+                        $('#updateTaskModal').modal('hide');
+                        alertSuccess(data.message);
+                        loadData();
+                    },
+                    error: function (e) {
+                        console.log("ERROR : ", e);
+                        $('#updateTaskModal').modal('hide');
+                        alertDanger(e);
+                    }
+                });
+
+            }
+        });
+
+        [taskName, deadLine, nhanVien, describeText].forEach(el => {
+            el.on('input', function () {
+                $('.alert-modal-container').html('');
+            });
+        });
+
+    });
+
+    $('input[type="file"]').change(function (e) {
+        var fileName = e.target.files[0].name;
+        $('.custom-file-label').html(fileName);
     });
 </script>
 
--- test.php
+++ test.php
@@ -1,3 +1,3 @@
 <?php
 require_once ('task_db.php');
-print_r(createNewTask(49, 3, 14));
+print_r(updateTask(58, 'task _ 1 _ test', 'task _ 1 _ test', '2022-01-09 12:00:00', '../file_gui/M33W7M5u/TimHieuVeSPA.docx', 14));
