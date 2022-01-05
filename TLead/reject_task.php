1da0cc69c06fb30ba0684bbc70374ad2d34ebaba
LeHieu11 <letrantrunghieu11@gmail.com>
2022-01-05 16:43:59 +0700

    Tlead/reject_task có vẻ như xong

--- TLead/reject_task.php
+++ TLead/reject_task.php
@@ -58,7 +58,7 @@
 <div class="container">
     <div class="row mb-2 flex-column-reverse flex-md-row">
         <div class="col-md-6 col-12 mb-4 align-items-center justify-content-end">
-            <h2 class="font-weight-bold text-left"><span id="tieude"></span></h2>
+            <h2 class="font-weight-bold text-left"><span id="title"></span></h2>
         </div>
         <div class="col-md-6 col-12 mb-4 d-flex justify-content-end align-items-center">
             <button class="btn btn-light">Trở về danh sách</button>
@@ -66,47 +66,35 @@
     </div>
     <div class="row p-4 bg-light rounded">
         <div class="col-12 d-flex align-items-center justify-content-between">
-            <p class="mb-0"><span id="hoten"></span> • <span id="datecreate"></span></p>
+            <p class="mb-0"><span id="tleadName"></span> <!--• <span id="datecreate">--></span></p>
             <span class="badge badge-danger px-3 py-2">Rejected</span>
         </div>
         <div class="col-12 mt-3 d-flex align-items-center justify-content-between">
-            <p class="mb-0">Đánh giá ___</p>
-            <p class="mb-0">Hạn nộp <span id="deadline"></span></p>
+            <p class="mb-0" id="dateCreate"></p>
+            <p class="mb-0" id="deadline">deadline</p>
         </div>
         <div style="margin : 30px 16px; border-bottom: 1px solid black; width: 100%"></div>
-        <div class="col-12">
-            Mô tả : <span id="mota"></span>
+        <div class="col-12" id="description">
+            Mô tả :
         </div>
         <div class="mt-4 col-md-8 col-lg-6">
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item d-flex justify-content-between align-items-center"><span id="supportfile"></span>
-                    <button class="btn btn-primary btn-sm">Download</button>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center"><span id="submitfile"></span>
-                    <button class="btn btn-primary btn-sm">Download</button>
-                </li>
-                <li class="list-group-item text-center">
-                    <a href="">Download All <i class="ml-2 fas fa-download"></i></a>
-                </li>
+            <ul class="list-group list-group-flush" id="supportFolderList">
+                <!-- List of support file -->
             </ul>
         </div>
         <div class="mt-4 col-md-4 col-lg-6">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Phần nộp công việc</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
+                    <p class="card-text" id="messageEmployee">Không có lời nhắn</p>
                     <div class="d-flex justify-content-between">
-                        <p class="card-text mb-0">Note.pdf</p>
-                        <button class="btn btn-primary btn-sm">Download</button>
+                        <p class="p-2 badge badge-secondary card-text mb-0" id="submitFolder">Không thấy file</p>
+                        <a id="downloadSubmitFile" class="text-light btn btn-primary btn-sm" download>Download</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-
-
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
@@ -119,53 +107,99 @@
             crossorigin="anonymous"></script>
 </body>
 
-<script>
-    $(document).ready(function () {
-        loadProduct();
+<script type="application/javascript">
+    $('input[type="file"]').change(function (e) {
+        var fileName = e.target.files[0].name;
+        $('.custom-file-label').html(fileName);
     });
 
-    function loadProduct(){
-        $.post("http://localhost/final/api/get_reject_task_detail.php",{id : "<?php echo $_GET['task']?>"}, function(data, status) {
-            data.data.forEach((task) => {
-                const tieude = document.getElementById('tieude');
-                tieude.innerHTML = task.TIEU_DE;
-                const hoten = document.getElementById('hoten');
-                hoten.innerHTML = task.HO_TEN;
-                const datecreate = document.getElementById('datecreate');
-                datecreate.innerHTML = task.DATE_CREATE;
-                const deadline = document.getElementById('deadline');
-                deadline.innerHTML = task.DEADLINE;
-                const mota = document.getElementById('mota');
-                mota.innerHTML = task.MO_TA;
-                const supportfile = document.getElementById('supportfile');
-                supportfile.innerHTML = shortcut(task.SUPPORT_FOLDER_PATH);
-                const submitfile = document.getElementById('submitfile');
-                submitfile.innerHTML = shortcut(task.SUBMIT_FOLDER_PATH);
-            })
-        },"json");
+    function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+        return false;
     }
 
-    function convert(time){
-        if (time == null){
-            return null;
-        }
-        else {
-            const date = time;
-            const readable_date = new Date(date).toLocaleDateString();
-            return readable_date;
-        }
+    function dateFormatForDeadLine(date){
+        let dateObj = new Date(date);
+        let month = dateObj.getMonth() + 1;
+        let day = dateObj.getDate();
+        let hours = dateObj.getHours();
+        let minute = dateObj.getMinutes();
+
+        newdate = day + "/" + month + " - " + hours + ":" + minute;
+        return newdate;
     }
 
-    function shortcut(file){
-        if (file == null){
-            return "Empty File";
-        }
-        else {
-            let text = file;
-            const myArray = text.split("/");
-            return myArray[3];
-        }
+    function dateFormatForCreate(date){
+        let dateObj = new Date(date);
+        let month = dateObj.getMonth() + 1;
+        let day = dateObj.getDate();
+
+        newdate = day + "/" + month;
+        return newdate;
     }
+
+    const fillData = function(){
+        let taskId = getUrlParameter('task');
+
+        $.get('http://localhost/final/API/get-task.php', {id: taskId}).done(function (respone) {
+            let taskInfo = respone.data[0];
+
+            $("#title").text(taskInfo.TIEU_DE);
+            $("#tleadName").text(taskInfo.HO_TEN);
+            $("#dateCreate").text("Ngày tạo: " + dateFormatForCreate(taskInfo.DATE_CREATE));
+            $("#deadline").text("Hạn nộp: " + dateFormatForDeadLine(taskInfo.DEADLINE));
+            $("#description").append(taskInfo.MO_TA);
+
+            if(taskInfo.message_employee){
+                $("#messageEmployee").text(taskInfo.message_employee);
+            }
+
+            let submitFileList = taskInfo.SUBMIT_FOLDER_PATH.split("+");
+
+            $("#downloadSubmitFile").attr("href", addDotToPathIfNeeded(submitFileList[submitFileList.length - 1]));
+
+            let fileNameIndex = submitFileList[submitFileList.length - 1].lastIndexOf("/") + 1;
+            let filename = submitFileList[submitFileList.length - 1].substr(fileNameIndex);
+
+            $("#submitFolder").text(filename);
+
+            $('#supportFolderList').html('');
+
+            let supportFileList = taskInfo.SUPPORT_FOLDER_PATH.split("+");
+
+            $.each(supportFileList, function( index, value ) {
+                let fileNameIndex = value.lastIndexOf("/") + 1;
+                let filename = value.substr(fileNameIndex);
+
+                $('#supportFolderList').append(
+                    `<li class="list-group-item d-flex justify-content-between align-items-center">${filename}
+                        <a href="${addDotToPathIfNeeded(value)}" class="text-light btn btn-primary btn-sm" download>Download</a>
+                    </li>`);
+            });
+        });
+    }
+
+    function addDotToPathIfNeeded(path){
+        if(path.substring(0, 3) != '../'){
+            return "../" + path;
+        }
+        else return path;
+    }
+
+    $(document).ready(function () {
+        fillData();
+    });
 </script>
 
 </html>
