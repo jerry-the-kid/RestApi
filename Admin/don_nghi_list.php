26dd0928cc0648f65c5d01646c245e7209174e48
Khoa-21501 <69575272+Khoa-21501@users.noreply.github.com>
2022-01-12 13:57:44 +0700

    Update don_nghi_list.php

--- Admin/don_nghi_list.php
+++ Admin/don_nghi_list.php
@@ -64,7 +64,7 @@ require_once('admin_validate.php');
 <div class="container">
     <div class="row">
         <div class="col-12 mb-4 align-items-center justify-content-end">
-            <h3 class="font-weight-bold">Danh Sách Nhiệm Vụ</h3>
+            <h3 class="font-weight-bold">Danh Sách Đơn Nghỉ Trưởng Phòng</h3>
         </div>
     </div>
 
@@ -151,6 +151,78 @@ require_once('admin_validate.php');
     $('input[type="file"]').change(function (e) {
         var fileName = e.target.files[0].name;
         $('.custom-file-label').html(fileName);
+    });
+</script>
+
+<script>
+    function removeVietnameseTones(str) {
+        str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g,"a");
+        str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g,"e");
+        str = str.replace(/ì|í|ị|ỉ|ĩ/g,"i");
+        str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g,"o");
+        str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g,"u");
+        str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g,"y");
+        str = str.replace(/đ/g,"d");
+        str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+        str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+        str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+        str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+        str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+        str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+        str = str.replace(/Đ/g, "D");
+        // Some system encode vietnamese combining accent as individual utf-8 characters
+        // Một vài bộ encode coi các dấu mũ, dấu chữ như một kí tự riêng biệt nên thêm hai dòng này
+        str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // ̀ ́ ̃ ̉ ̣  huyền, sắc, ngã, hỏi, nặng
+        str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // ˆ ̆ ̛  Â, Ê, Ă, Ơ, Ư
+        // Remove extra spaces
+        // Bỏ các khoảng trắng liền nhau
+        str = str.replace(/ + /g," ");
+        str = str.trim();
+        // Remove punctuations
+        // Bỏ dấu câu, kí tự đặc biệt
+        str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g," ");
+        return str;
+    }
+
+    function check(status){
+        if (status == "approved"){
+            return "badge badge-success p-2";
+        }
+        else if (status == "refused"){
+            return "badge badge-secondary p-2";
+        }
+        else if (status == "waiting"){
+            return "badge badge-warning p-2";
+        }
+    }
+
+    let searchData = [];
+    const renderTable = function (data){
+        $('#table-body').html('');
+        data.forEach((info) => {
+            $('#table-body').append(`
+                    <tr>
+                        <td>${info.NOI_DUNG}</td>
+                        <td>NV${info.MA_NV} - ${info.HO_TEN}</td>
+                        <td>${info.SO_NGAY}</td>
+                        <td><span class="${check(info.TRANG_THAI)}">${info.TRANG_THAI}</span></td>
+                        <td><a href="don_nghi.php?madonnghi=${info.MA_NGHI}" style="text-decoration: none">Xem chi tiết</a></td>
+                    </tr>`
+            );
+        });
+    }
+
+
+    const loadData = function () {
+        $.get('http://localhost:81/final/API/get_donnghi_teamLeaders.php').done(function (respone) {
+            const {data} = respone;
+            renderTable(data);
+            searchData = data;
+        });
+    }
+
+    $(document).ready(function () {
+        loadData();
     });
 </script>
 </html>
